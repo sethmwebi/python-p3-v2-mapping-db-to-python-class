9e2c7c9d7d9d945d02a146b39c366f800926f0e1
Seth Mwebi <sethmwebi27@gmail.com>
2024-09-16 10:54:08 +0300

    mapping db to python class

--- lib/department.py
+++ lib/department.py
@@ -1,7 +1,10 @@
-from __init__ import CURSOR, CONN
+from __init__ import CONN, CURSOR
 
 
 class Department:
+
+    # Dictionary of objects saved to the database
+    all = {}
 
     def __init__(self, name, location, id=None):
         self.id = id
@@ -13,7 +16,7 @@ class Department:
 
     @classmethod
     def create_table(cls):
-        """ Create a new table to persist the attributes of Department instances """
+        """Create a new table to persist the attributes of Department instances"""
         sql = """
             CREATE TABLE IF NOT EXISTS departments (
             id INTEGER PRIMARY KEY,
@@ -25,7 +28,7 @@ class Department:
 
     @classmethod
     def drop_table(cls):
-        """ Drop the table that persists Department instances """
+        """Drop the table that persists Department instances"""
         sql = """
             DROP TABLE IF EXISTS departments;
         """
@@ -33,7 +36,7 @@ class Department:
         CONN.commit()
 
     def save(self):
-        """ Insert a new row with the name and location values of the current Department instance.
+        """Insert a new row with the name and location values of the current Department instance.
         Update object id attribute using the primary key value of new row.
         """
         sql = """
@@ -45,10 +48,11 @@ class Department:
         CONN.commit()
 
         self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self
 
     @classmethod
     def create(cls, name, location):
-        """ Initialize a new Department instance and save the object to the database """
+        """Initialize a new Department instance and save the object to the database"""
         department = cls(name, location)
         department.save()
         return department
@@ -63,12 +67,55 @@ class Department:
         CURSOR.execute(sql, (self.name, self.location, self.id))
         CONN.commit()
 
-    def delete(self):
-        """Delete the table row corresponding to the current Department instance"""
-        sql = """
-            DELETE FROM departments
-            WHERE id = ?
-        """
+    @classmethod
+    def instance_from_db(cls, row):
+        """Return a Department object having the attribute values from the table row."""
 
+        # check the dictionary for an existing instance using the row's primary key
+        department = cls.all.get(row[0])
+        if department:
+            # ensure attributes match row values in case local object was modified
+            department.name = row[1]
+            department.location = row[2]
+        else:
+            # not in dictionary, create new instance and add to dictionary
+            department = cls(row[1], row[2])
+            department.id = row[0]
+            cls.all[department.id] = department
+        return department
+
+    @classmethod
+    def get_all(cls):
+        """Return a list containing a Department object per row in the table"""
+        sql = """SELECT * FROM departments"""
+        rows = CURSOR.execute(sql).fetchall()
+
+        return [cls.instance_from_db(row) for row in rows]
+
+    @classmethod
+    def find_by_id(cls, id):
+        """Return a department object corresponding to the table row matching the specified primary key"""
+        sql = """SELECT * FROM departments WHERE id = ?"""
+
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        return cls.instance_from_db(row) if row else None
+
+    @classmethod
+    def find_by_name(cls, name):
+        """Return a department object corresponding to the first table row matching specified name"""
+        sql = """SELECT * FROM departments WHERE name is ?"""
+
+        row = CURSOR.execute(sql, (name,)).fetchone()
+        return cls.instance_from_db(row) if row else None
+
+    def delete(self):
+        """Delete the table row corresponding to the current Department instance, delete the dictionary entry , and re-assign id attribute"""
+        sql = """DELETE FROM departments WHERE id = ?"""
         CURSOR.execute(sql, (self.id,))
         CONN.commit()
+
+        # Delete the dictionary entry using id as the key
+        del type(self).all[self.id]
+
+        # set the id to None
+        self.id = None
